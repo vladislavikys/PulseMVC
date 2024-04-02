0b61f2de001a3f1267daf1dcfe67455ec3a7c4f2
vladISlove <>
2024-04-02 13:55:56 +0300

    Implement basic pulse reading functionality

--- PulseMVC/Screens/HeartRate/Controller/PulseViewController.swift
+++ PulseMVC/Screens/HeartRate/Controller/PulseViewController.swift
@@ -2,7 +2,6 @@ import UIKit
 import CoreData
 import AVKit
 
-//ЖОПА
 class PulseViewController: BaseViewController {
     private var buttonInfo = UIButton()
     private var pulseStatusLabel = UILabel()
@@ -19,8 +18,18 @@ class PulseViewController: BaseViewController {
     private var cameraView = CameraAccessView()
     var dimmy = DimmyView()
     var userProfile = AboutModel()
+
+    //таймер для прогресса
+    private var progressTimer: Timer?
+    private var currentProgress: CGFloat = 0.0
     
     
+    
+    // Менеджеры для работы с измерением пульса
+    var hueFilter = Filter() // Фильтр для обработки изображения
+    var pulseDetector = PulseDetector() // Детектор пульса
+    var inputs: [CGFloat] = [] // Входные данные для детектора пульса
+    var measurementStartedFlag = false // Флаг начала измерения
     var heartRateManager: HeartRateManager! // Менеджер для измерения сердечного ритма
     var bpmForCalculating: [Int] = [] // Массив для хранения значений BPM
     var validFrameCounter = 0 // Счетчик валидных кадров
@@ -28,9 +37,6 @@ class PulseViewController: BaseViewController {
     var timerTwo = Timer() // Второй таймер
 
     
-    //таймер для прогресса
-    private var progressTimer: Timer?
-    private var currentProgress: CGFloat = 0.0
 
     
     override func viewDidLoad() {
@@ -166,10 +172,7 @@ class PulseViewController: BaseViewController {
                 print("Возраст: \(coreData.age)")
                 print("-----------------")
             }
-            
-            
-            //settings start measure func this
-            startMeasurePulseView()
+            startPulseHeartRate()
         } else {
             print("Первый раз вошел - startTapped")
             UserDefaults.standard.set(true, forKey: "userEnteringApp")
@@ -254,15 +257,6 @@ extension PulseViewController {
         }
     }
     
-    func startMeasurePulseView(){
-        fingerOnCameraGuide.isHidden = false
-        heartbeatGraphView.isHidden = true
-        startButton.isHidden = true
-        cameraFingerGuideText.isHidden = false
-        
-    
-    }
-    
     func endMeasurePulseView(){
         fingerOnCameraGuide.isHidden = true
         heartbeatGraphView.isHidden = false
@@ -274,5 +268,226 @@ extension PulseViewController {
 extension PulseViewController: AboutMeViewControllerDelegate {
     func didCloseAboutScreen() {
         showCameraView()
+    }
+}
+
+extension PulseViewController {
+    private func startPulseHeartRate() {
+        fingerOnCameraGuide.isHidden = false
+        heartbeatGraphView.isHidden = true
+        startButton.isHidden = true
+        cameraFingerGuideText.isHidden = false
+        
+        if AVCaptureDevice.authorizationStatus(for: .video) == .authorized {
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                // Если разрешение получено, останавливаем текущую анимацию,
+                // показываем необходимые UI элементы и инициализируем процесс захвата видео.
+                print("START PULSE MEASURE")
+            }
+        } else {
+            // Если разрешение на использование камеры не получено,
+            //показать алерт на получение разрешения камеры
+        }
+        
+    }
+    
+    // Инициализирует процесс захвата видео для начала измерения пульса.
+    private func initStartPulse() {
+        // Инициализация захвата видео и сессии захвата.
+        initVideoCapture()
+        initCaptureSession()
+    }
+
+    // Инициализация захвата видео с камеры.
+    func initVideoCapture() {
+        // Задаем характеристики захвата видео.
+        let specs = VideoSpec(fps: 60, size: CGSize(width: 50, height: 50))
+        // Инициализация менеджера сердечного ритма с выбранной камерой и характеристиками.
+        heartRateManager = HeartRateManager(cameraType: .back, preferredSpec: specs, previewContainer: CAShapeLayer())
+        // Обработчик получаемых изображений.
+        heartRateManager.imageBufferHandler = { [unowned self] (imageBuffer) in
+            self.handle(buffer: imageBuffer)
+        }
+    }
+    
+    // Начало сессии захвата видео.
+    func initCaptureSession() {
+        heartRateManager.startCapture()
+    }
+    
+    // Завершение сессии захвата видео.
+    func deinitCaptureSession() {
+        if heartRateManager != nil {
+            heartRateManager.stopCapture()
+            toggleTorch(status: false)
+        }
+    }
+    
+    // Включение или выключение вспышки.
+    func toggleTorch(status: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        device.toggleTorch(on: status)
+    }
+    
+    // Начало процесса измерения пульса.
+    func startMeasurement() {
+        DispatchQueue.main.async { [unowned self] in
+            self.bpmForCalculating.removeAll()
+            self.toggleTorch(status: true)
+            //self.progress.animationProgress(duration: 20)
+            var count = 20
+            
+            // Таймер для обновления интерфейса и расчета пульса.
+            self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] (timer) in
+                guard let self = self else { return }
+                
+                // Логика работы таймера.
+                if count > 0 {
+                    count -= 1
+                    let average = self.pulseDetector.getAverage()
+                    let pulse = 60.0/average
+                    print("pulse: \(pulse)")
+                    
+                    // Проверка пульса и обновление данных.
+                    if pulse != -60 {
+                        self.pulseStackView.pulseLabel.text = "\(lroundf(pulse))"
+                        self.bpmForCalculating.append(Int(pulse))
+                    }
+                } else {
+                    // Действия по завершении измерения.
+                    if self.bpmForCalculating != [] {
+//                        self.viewModel?.calculatingAndSaveToDBBpmData(pulse: self.bpmForCalculating)
+//                        self.viewModel?.callAnalyzeModule(self)
+                    }
+                    self.defaultState()
+                }
+            })
+        }
+    }
+    
+    // Установка состояния по умолчанию для UI элементов.
+    func defaultState() {
+        tabBarController?.tabBar.isHidden = false
+        self.startButton.isHidden = false
+        self.pulseStackView.isHidden = false
+        self.buttonInfo.isHidden = false
+        
+        print("DEFALT SCREEN ")
+        
+        
+        self.toggleTorch(status: false)
+        self.heartRateManager.stopCapture()
+        timer.invalidate()
+    }
+    // Обработка захваченного видео для измерения пульса.
+    func handle(buffer: CMSampleBuffer) {
+        var redmean:CGFloat = 0.0
+        var greenmean:CGFloat = 0.0
+        var bluemean:CGFloat = 0.0
+        
+        // Получение изображения из буфера и преобразование его в цветовые значения.
+        let pixelBuffer = CMSampleBufferGetImageBuffer(buffer)
+        let cameraImage = CIImage(cvPixelBuffer: pixelBuffer!)
+        
+        // Вычисление среднего значения цвета пикселей.
+        let extent = cameraImage.extent
+        let inputExtent = CIVector(x: extent.origin.x, y: extent.origin.y, z: extent.size.width, w: extent.size.height)
+        let averageFilter = CIFilter(name: "CIAreaAverage",
+                                     parameters: [kCIInputImageKey: cameraImage, kCIInputExtentKey: inputExtent])!
+        let outputImage = averageFilter.outputImage!
+        
+        let ctx = CIContext(options:nil)
+        let cgImage = ctx.createCGImage(outputImage, from:outputImage.extent)!
+        
+        let rawData:NSData = cgImage.dataProvider!.data!
+        let pixels = rawData.bytes.assumingMemoryBound(to: UInt8.self)
+        let bytes = UnsafeBufferPointer<UInt8>(start:pixels, count:rawData.length)
+        var BGRA_index = 0
+        for pixel in UnsafeBufferPointer(start: bytes.baseAddress, count: bytes.count) {
+            switch BGRA_index {
+            case 0:
+                bluemean = CGFloat (pixel)
+            case 1:
+                greenmean = CGFloat (pixel)
+            case 2:
+                redmean = CGFloat (pixel)
+            case 3:
+                break
+            default:
+                break
+            }
+            BGRA_index += 1
+        }
+        // Преобразование цветового пространства из RGB в HSV
+        let hsv = rgb2hsv((red: redmean, green: greenmean, blue: bluemean, alpha: 1.0))
+        
+        // Проверяем, что над камерой находится палец или что-то подобное
+        if (hsv.1 > 0.5 && hsv.2 > 0.5) {
+            // Выполняем операции на главной очереди асинхронно
+            DispatchQueue.main.async {
+                // Включаем фонарик, если он не включен
+                self.toggleTorch(status: true)
+                // Если измерение пульса еще не начато, запускаем его
+                if !self.measurementStartedFlag {
+                    self.startMeasurement()
+                    self.measurementStartedFlag = true
+                }
+            }
+            // Увеличиваем счетчик валидных кадров
+            validFrameCounter += 1
+            // Добавляем значение тона в массив
+            inputs.append(hsv.0)
+            // Фильтруем значение тона
+            let filtered = hueFilter.processValue(value: Double(hsv.0))
+            // Если количество валидных кадров больше 60, добавляем значение в анализатор пульса
+            if validFrameCounter > 60 {
+                _ = self.pulseDetector.addNewValue(newVal: filtered, atTime: CACurrentMediaTime())
+            }
+        } else {
+            // Отключаем таймер
+            self.timer.invalidate()
+            // Сбрасываем счетчик валидных кадров, флаг и анализатор пульса
+            validFrameCounter = 0
+            measurementStartedFlag = false
+            pulseDetector.reset()
+            // Выполняем операции на главной очереди
+            DispatchQueue.main.async {
+                // Останавливаем анимацию пульса и сердцебиения
+                //self.stopAnimationPulse()
+                //self.stopAnimationHeartBeat()
+                // Изменяем текст метки "fingerLabel"
+                // Обновляем значение переменной "rateVariable"
+                self.pulseStackView.pulseLabel.text = "00"
+                // Удаляем анимации прогресса
+                //self.progress.deleteAnimations()
+            }
+        }
+    }
+    
+    // Сохраняет результаты измерения пульса в базу данных и закрывает экран с результатами.
+    func saveToDBAndCloseResultView() {
+        // Вызываем метод модели представления для сохранения результатов измерений пульса в базу данных.
+        viewModel?.saveToBDBpmSetting()
+
+        // После сохранения результатов, скрываем нижнюю навигационную панель,
+        // чтобы вернуть пользователя к основному пользовательскому интерфейсу приложения.
+        tabBarController?.tabBar.isHidden = false
+
+        // Проверяем, был ли уже показан экран оценки приложения пользователем.
+        // GetUserSettings().showRating возвращает false, если экран оценки еще не показывался.
+        if !GetUserSettings().showRating {
+            // Если экран оценки не показывался, создаем и настраиваем view для отображения запроса оценки приложения.
+            self.ratingView.delegate = self  // Устанавливаем делегат для обработки действий пользователя на экране оценки.
+            self.tabBarController?.view.addSubview(ratingView)  // Добавляем экран оценки на текущий таб бар контроллер для отображения пользователю.
+
+            // Вызываем метод модели представления для изменения состояния,
+            // указывающего, что экран оценки уже был показан пользователю.
+            viewModel?.changeStateRatingShow()
+        } else {
+            // Если экран оценки уже был показан, переключаем вкладку таб бара на следующую,
+            // чтобы пользователь продолжил взаимодействие с приложением.
+            tabBarController?.selectedIndex = 1
+        }
     }
 }
